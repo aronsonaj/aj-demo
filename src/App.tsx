273a314890c81a387e1be002cc398361853024ce
User <user@Arnolds-MacBook-Pro.local>
2024-12-19 15:19:42 -0800

    delete dotfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+// declare global {
+//   interface Window {
+//     dataLayer: any[];
+//   }
+// }
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './components/ui/card'
 import { Button } from './components/ui/button'
